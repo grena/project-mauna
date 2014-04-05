fc098b1efc17adaf1cc0ae0db34abbfa5628dad9
Freyskeyd <contact@freyskeyd.fr>
2014-04-05 17:22:41 +0200

    initialize protractor test

---
+++ tests/e2e/unlog/unlog-navigation.js
@@ -0,0 +1,28 @@
+describe('unlog homepage', function() {
+    'use strict';
+
+    var fail = function () {
+        expect(true).toBe(false);
+    };
+
+    it('should rediect to login page if access to root', function() {
+        browser.get('/#/login');
+        var loginURL = browser.getCurrentUrl();
+
+        browser.get('/');
+        expect(browser.getCurrentUrl()).toEqual(loginURL);
+    });
+
+    it('should rediect to login page if access to any page and not authenticated', function() {
+        browser.get('/#/login');
+        var loginURL = browser.getCurrentUrl();
+
+        browser.get('/#/');
+        expect(browser.getCurrentUrl()).toEqual(loginURL);
+    });
+
+    it('should warn on missing/malformed credentials', fail);
+    it('should accept a valid email address and password', fail);
+    it('should return to the login page after logout', fail);
+    it('should return to the home page after login', fail);
+});
