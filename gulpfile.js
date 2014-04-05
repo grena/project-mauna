30334ae505e94065b76206041699f070991689c6
Freyskeyd <contact@freyskeyd.fr>
2014-04-05 16:18:52 +0200

    help command

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,16 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    glob = require('glob');
+
+/*
+|--------------------------------------------------------------------------
+| Fetch all tasks
+|--------------------------------------------------------------------------
+*/
+var files = glob.sync('./tasks/**/*.js');
+files.forEach(function (file) {
+    'use strict';
+    require(file)(gulp);
+});
+
 
 gulp.task('default', ['help']);
