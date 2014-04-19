5c1509e9ca7e20ebd51752cc87e943c57824f269
Freyskeyd <contact@freyskeyd.fr>
2014-04-19 11:44:07 +0200

    Upgrading to laravel latest stable with security fix

--- app/models/User.php
+++ app/models/User.php
@@ -67,4 +67,24 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
         return $this->hasMany('Settler');
     }
 
+	/*
+	|--------------------------------------------------------------------------
+	| Security Token fix 4.1.26
+	|--------------------------------------------------------------------------
+	*/
+	public function getRememberToken()
+	{
+	    return $this->remember_token;
+	}
+
+	public function setRememberToken($value)
+	{
+	    $this->remember_token = $value;
+	}
+
+	public function getRememberTokenName()
+	{
+	    return 'remember_token';
+	}
+
 }
