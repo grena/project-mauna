199e2cc1db6f6a435d9914dcefab55ca81ef95c4
Freyskeyd <contact@freyskeyd.fr>
2014-04-20 17:28:30 +0200

    add config file

--- app/config/asteroids.php
+++ app/config/asteroids.php
@@ -14,44 +14,44 @@ return [
 
     'types' => [
         [
-            'key' => 't1',
-            'name' => 'BETA',
-            'rarity' => 82,
+            'key'                => 't1',
+            'name'               => 'BETA',
+            'rarity'             => 82,
             'mineral_multiplier' => 1,
-            'radioactivity' => [
+            'radioactivity'      => [
                 70 => [1, 2],
                 25 => [2, 4],
                 5  => [5, 5]
             ],
         ],
         [
-            'key' => 't2',
-            'name' => 'Classe TITAN',
-            'rarity' => 16,
+            'key'                => 't2',
+            'name'               => 'Classe TITAN',
+            'rarity'             => 16,
             'mineral_multiplier' => 2.3,
-            'radioactivity' => [
+            'radioactivity'      => [
                 70 => [4, 7],
                 25 => [7, 9],
                 5  => [11, 11]
             ],
         ],
         [
-            'key' => 't3',
-            'name' => 'Géocroiseur',
-            'rarity' => 1.7,
+            'key'                => 't3',
+            'name'               => 'Géocroiseur',
+            'rarity'             => 1.7,
             'mineral_multiplier' => 5.8,
-            'radioactivity' => [
+            'radioactivity'      => [
                 70 => [7, 9],
                 25 => [10, 12],
                 5  => [14, 14]
             ],
         ],
         [
-            'key' => 't4',
-            'name' => 'Archonte T30',
-            'rarity' => 0.3,
+            'key'                => 't4',
+            'name'               => 'Archonte T30',
+            'rarity'             => 0.3,
             'mineral_multiplier' => 12,
-            'radioactivity' => [
+            'radioactivity'      => [
                 70 => [15, 18],
                 25 => [19, 22],
                 5  => [25, 25]
